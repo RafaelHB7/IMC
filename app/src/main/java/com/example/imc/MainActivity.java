d207402b3ea423df4071b0b632ad072aea6d7bc9
RafaelHB7 <rafael.hobi9@gmail.com>
2020-03-03 21:48:12 -0300

    Adicionado feedback visual para os tipos de IMC Melhora em componentes visuais

--- app/src/main/java/com/example/imc/MainActivity.java
+++ app/src/main/java/com/example/imc/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText peso;
     EditText altura;
-    Button botaoCalcular;
     TextView saida;
+    ImageView imagem;
+    SeekBar barra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         peso = findViewById(R.id.Peso);
         altura = findViewById(R.id.Altura);
-        botaoCalcular = findViewById(R.id.BotaoCalcular);
         saida = findViewById(R.id.imc);
+        imagem = findViewById(R.id.imagem);
+        barra = findViewById(R.id.seekBar);
     }
 
     public float CalcularIMC(float peso, float altura){
@@ -33,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
         float peso = Float.parseFloat(this.peso.getText().toString());
         float altura = Float.parseFloat(this.altura.getText().toString());
         float resultado = CalcularIMC(peso, altura);
-        saida.setText(String.format("%.2f",resultado));
+
+        saida.setText(String.format("%.2f", resultado));
+        barra.setProgress((int) (resultado));
+
+        if (resultado < 18.5) {
+            imagem.setImageResource(R.drawable.abaixopeso);
+        } else if (resultado < 25) {
+            imagem.setImageResource(R.drawable.normal);
+        } else if (resultado < 30) {
+            imagem.setImageResource(R.drawable.sobrepeso);
+        } else if (resultado < 35) {
+            imagem.setImageResource(R.drawable.obesidade1);
+        } else if (resultado < 40) {
+            imagem.setImageResource(R.drawable.obesidade2);
+        } else {
+            imagem.setImageResource(R.drawable.obesidade3);
+        }
     }
 }
